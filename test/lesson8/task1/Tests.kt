7cc9fdd48446e35053354263ce84a6d3d6561b71
maratdin7 <maratdin7@gmail.com>
2018-12-13 21:37:56 +0300

    8 урок

--- test/lesson8/task1/Tests.kt
+++ test/lesson8/task1/Tests.kt
@@ -172,6 +172,7 @@ class Tests {
 	@Test
 	@Tag("Normal")
 	fun lineByPoint() {
+
 		assertApproxEquals(Line(Point(0.0, 0.0), PI / 2), lineByPoints(Point(0.0, 0.0), Point(0.0, 2.0)))
 		assertApproxEquals(Line(Point(1.0, 1.0), PI / 4), lineByPoints(Point(1.0, 1.0), Point(3.0, 3.0)))
 		assertEquals(2.356194490192345, lineByPoints(Point(5e-324, -632.0), Point(-632.0, 5e-324)).angle)
